6e18ac57859d4ad853e6b0ecd775d9e424be8253
Rajendra Kadam <raj.725@outlook.com>
2024-03-08 18:15:09 +0530

    Add PebbloRetrievalQA Chain with Identity & Semantic Enforcement support

--- libs/langchain/langchain/chains/retrieval_qa/base.py
+++ libs/langchain/langchain/chains/retrieval_qa/base.py
@@ -300,3 +300,48 @@ class VectorDBQA(BaseRetrievalQA):
     def _chain_type(self) -> str:
         """Return the chain type."""
         return "vector_db_qa"
+
+
+class PebbloRetrievalQA(RetrievalQA):
+    """Retrieval Chain with Identity & Semantic Enforcement for question-answering against a vector database."""
+
+    auth_context: dict = Field(exclude=True)
+    """Auth context to use in the retriever."""
+
+    def _get_docs(
+        self,
+        question: str,
+        *,
+        run_manager: CallbackManagerForChainRun,
+    ) -> List[Document]:
+        """Get docs."""
+
+        #  Identity Enforcement
+        search_kwargs = self.retriever.search_kwargs
+        if (
+            "filter" in search_kwargs
+            and "authorized_identities" in search_kwargs["filter"]
+        ):
+            raise ValueError(
+                "authorized_identities already exists in search_kwargs['filter']"
+            )
+        search_kwargs.setdefault("filter", {})[
+            "authorized_identities"
+        ] = self.auth_context
+
+        docs = super()._get_docs(question, run_manager=run_manager)
+        return docs
+
+    async def _aget_docs(
+        self,
+        question: str,
+        *,
+        run_manager: AsyncCallbackManagerForChainRun,
+    ) -> List[Document]:
+        """Get docs."""
+        raise NotImplementedError("PebbloRetrievalQA does not support async")
+
+    @property
+    def _chain_type(self) -> str:
+        """Return the chain type."""
+        return "pebblo_retrieval_qa"
